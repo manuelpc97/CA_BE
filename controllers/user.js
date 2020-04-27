f401dfa54e32b47d7f4bf24ea068cdbc2ce12eec
manuelpc97 <mcanelo41@gmail.com>
2020-04-26 19:14:03 -0600

    Adding validation to user controllers and changing promises to async/await

--- controllers/user.js
+++ controllers/user.js
@@ -1,30 +1,39 @@
 var user = require('../schemas/user');
 
-exports.createUser = (request, response) => {
-    var newUser = new user({
-        username : request.body.username, 
-        password: request.body.password,
-        firstName: request.body.firstName,
-        lastName: request.body.lastName,
-        email: request.body.email, 
-        id: request.body.id, 
-        phone: request.body.phone
-    });
-    
-    newUser.save();
-    response.sendStatus(200);
+exports.createUser = async (request, response) => {
+    try{
+        var newUser = new user({
+            username : request.body.username, 
+            password: request.body.password,
+            firstName: request.body.firstName,
+            lastName: request.body.lastName,
+            email: request.body.email, 
+            id: request.body.id, 
+            phone: request.body.phone
+        });
+        
+        await newUser.save();
+        response.sendStatus(200);
+    }catch(exception){
+        response.status(500).send(exception);
+    }
 }
 
-exports.logIn = (request, response) => {
-    user.find({username: request.body.username}, (error, filteredUser) => {
-        if(error){
-            response.sendStatus(500);
-        }else{
-            if(filteredUser[0] && filteredUser[0].password === request.body.password){
-                response.json(filteredUser[0]);
+exports.logIn = async (request, response) => {
+    try{
+        var users = await user.find({username: request.body.username});
+        
+        if(users.length !== 0){
+            var filteredUser = users[0];
+            if(filteredUser && filteredUser.password === request.body.password){
+                response.status(200).json(filteredUser);
             }else{
-                response.sendStatus(401);
+                response.status(401).send('User not found');
             }
+        }else{
+            response.status(401).send("User not found");;
         }
-    })
+    }catch(exception){
+        response.status(500).send(exception);
+    }
 }
