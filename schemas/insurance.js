28d5c8b38c9028a1ce972608c5fdfb5f1ca5c075
manuelpc97 <mcanelo41@gmail.com>
2020-06-14 16:58:51 -0600

    Adding form tu insurance

--- schemas/insurance.js
+++ schemas/insurance.js
@@ -3,7 +3,8 @@ var validator = require('mongoose-unique-validator');
 
 var insurance = mongoose.Schema({
     name: {type: String, require: true, unique: true},
-    type: {type: String, require: true}
+    type: {type: String, require: true},
+    form: {type: String}
 });
 
 insurance.plugin(validator);
