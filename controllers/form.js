3ee9009c057cfc022e6b1b7e2331f19e32948e14
DanielaOdileth <odilethpadilla@unitec.edu>
2020-07-02 20:53:23 -0600

    function to save form

--- controllers/form.js
+++ controllers/form.js
@@ -1,4 +1,5 @@
 const formModel = require('../schemas/form');
+const FilledForm = require('../schemas/filledForms');
 
 exports.getFormById = async (request, response) => {
     const id = request.params.id;
@@ -7,5 +8,21 @@ exports.getFormById = async (request, response) => {
         response.status(200).send(form);
     } catch (error) {
         response.send(`There was an error to get business ${error}`);
+    }
+}
+
+exports.saveFilledForm = async (req, res) => {
+    const { filledForm, userId, productId, timestamp } = req.body;
+    try {
+        const newFilledForm = new FilledForm({
+            filledForm,
+            userId,
+            productId,
+            timestamp
+        });
+        await newFilledForm.save();
+        res.status(200).send({message: `Filled form was saved`});
+    } catch (error) {
+        res.status(500).send(`There was an error to save filled form ${error}`)
     }
 }
