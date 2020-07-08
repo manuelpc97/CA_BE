7aacb862a3f09e460238290427c4ffdfbe0665c2
DanielaOdileth <odilethpadilla@unitec.edu>
2020-07-07 22:49:34 -0600

    adding function to get filled forms

--- controllers/form.js
+++ controllers/form.js
@@ -1,5 +1,9 @@
 const formModel = require('../schemas/form');
 const FilledForm = require('../schemas/filledForms');
+const productModel = require('../schemas/product');
+const businessModel = require('../schemas/business');
+const insuranceModel = require('../schemas/insurance');
+const moment = require('moment-timezone');
 
 exports.getFormById = async (request, response) => {
     const id = request.params.id;
@@ -23,8 +27,31 @@ exports.saveFilledForm = async (req, res) => {
     try {
         const newFilledForm = new FilledForm(value);
         await newFilledForm.save();
-        res.status(200).send({message: `Filled form was saved`});
+        res.status(200).send({ message: `Filled form was saved` });
     } catch (error) {
         res.status(500).send(`There was an error to save filled form ${error}`)
+    }
+}
+
+exports.getProductFilledForms = async (req, res) => {
+    const { userId } = req.params;
+    try {
+        const filledForms = await FilledForm.find({ userId, productId: { $exists: true } });
+        const data = filledForms.map(async form => {
+            const { timestamp, productId } = form;
+            const { business, insurance, yearPayment } = await productModel.findById({ _id: productId });
+            const { name: businessName } = await businessModel.findById({ _id: business });
+            const { name: insuranceName } = await insuranceModel.findById({ _id: insurance });
+            return {
+                timestamp: moment.tz(timestamp, 'America/Tegucigalpa').format('DD/MM/YYYY'),
+                yearPayment,
+                businessName,
+                insuranceName
+            }
+        });
+        const productsObtained = await Promise.all(data);
+        res.status(200).send({ totalProducts: filledForms.length, productsObtained });
+    } catch (error) {
+        res.status(500).send(`There was en error to get filled forms ${error}`)
     }
 }
--- routes/form.js
+++ routes/form.js
@@ -4,5 +4,6 @@ var formController = require('../controllers/form');
 
 router.get('/get/:id', formController.getFormById);
 router.post('/saveFilled', formController.saveFilledForm);
+router.get('/get/userForms/:userId', formController.getProductFilledForms);
 
 module.exports = router;
